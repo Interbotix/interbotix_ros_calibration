8e23e2ba4c5e5c2b2250676ac644ba6cfd9871f1
pinakjani <pinakjani99@gmail.com>
2023-08-09 16:58:51 -0400

    [Launch] Added Launch file for perception

--- hand_eye_calibration/setup.py
+++ hand_eye_calibration/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'hand_eye_calibration'
 
@@ -10,13 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='pj',
+    maintainer='Pinak Jani',
     maintainer_email='pinakjani99@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='The interbotix_ros_calibration package',
+    license='BSD',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
